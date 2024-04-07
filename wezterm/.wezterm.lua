dfee248ccd4c5b74c2fa3e52581a3b035411d177
zish-rob-crur <zish-rob-crur@gmail.com>
2024-04-07 13:52:40 +0800

    Update wezterm theme and appearance handling

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -2,26 +2,51 @@ local wezterm = require 'wezterm'
 local mux = wezterm.mux
 local config = wezterm.config_builder()
 
-local DarkTheme = "Flexoki Dark"
+local DarkTheme = "Tokyo Night (Gogh)"
+
+local LightTheme = "Catppuccin Latte"
 
 function get_colordir()
-  local path_sep = package.config:sub(1,1) 
-  local color_dir = os.getenv("HOME") or os.getenv("USERPROFILE") 
-  color_dir = color_dir .. path_sep .. "GitHubRepos" .. path_sep .. "dotfiles" .. path_sep .. "wezterm" .. path_sep .. "colors"
+  local path_sep = package.config:sub(1, 1)
+  local color_dir = os.getenv("HOME") or os.getenv("USERPROFILE")
+  color_dir = color_dir ..
+      path_sep .. "GitHubRepos" .. path_sep .. "dotfiles" .. path_sep .. "wezterm" .. path_sep .. "colors"
   return color_dir
 end
 
-function get_theme()
-  return DarkTheme
+function get_appearance()
+  if wezterm.gui then
+    return wezterm.gui.get_appearance()
+  end
+  return 'Dark'
 end
+
+function scheme_for_appearance(appearance)
+  if appearance:find 'Dark' then
+    return DarkTheme
+  else
+    return LightTheme
+  end
+end
+
+wezterm.on("update-right-status", function(window, pane)
+  local overrides = {}
+  if wezterm.gui.get_appearance() == "Light" then
+    overrides.theme = LightTheme
+  else
+    overrides.theme = DarkTheme
+  end
+  window:set_config_overrides(overrides)
+end
+)
 
 config.font = wezterm.font('JetBrains Mono')
 config.color_scheme_dirs = { get_colordir() }
-config.color_scheme = get_theme()
+config.color_scheme = scheme_for_appearance(get_appearance())
 config.window_decorations = "RESIZE"
 config.font_size = 16.0
 config.show_new_tab_button_in_tab_bar = false
-config.hide_tab_bar_if_only_one_tab = true 
+config.hide_tab_bar_if_only_one_tab = true
 config.window_background_opacity = 0.97
 
 config.use_fancy_tab_bar = false
